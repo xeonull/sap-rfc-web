b8dcd67a3ddcebab4fcfb793d2fedd136d3b7c45
xeonull <xeonull@gmail.com>
2023-10-21 11:19:06 +0300

    upd: formatting app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import sapRouter from "./route/sap.route.js";
+import express from 'express';
+import sapRouter from './route/sap.route.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,15 +7,15 @@ const app = express();
 
 // Add CORS:
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", " http://127.0.0.1:3001");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3001');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 // Add routes:
-app.use("/api", sapRouter);
+app.use('/api', sapRouter);
 
 app.listen(PORT, function () {
-  console.log("Server started...");
+  console.log('Server started...');
 });
